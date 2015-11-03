d8e3f331bad4d1a9f4b018391508f59fb07dd464
Carey Fong <vicofpf@outlook.com>
2015-11-03 13:40:24 +0800

    绘画函数拆分

--- TracePanel/TracePanel.cs
+++ TracePanel/TracePanel.cs
@@ -21,11 +21,21 @@ namespace TraceCtrlLib
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            Graphics dc = e.Graphics;
-            Pen bluePen = new Pen(Color.Blue, 3);
-            dc.DrawRectangle(bluePen, 0, 0, 50, 50);
-            Pen redPen = new Pen(Color.Red, 2);
-            dc.DrawEllipse(redPen, 0, 50, 80, 60);
+            DrawBorder();
+            DrawText();
         }
+
+        #region "Drawing"
+
+        private void DrawBorder()
+        {
+            
+        }
+
+        private void DrawText()
+        {
+            
+        }
+        #endregion
     }
 }
