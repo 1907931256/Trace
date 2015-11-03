73a57cd8d418d63a2968485d64860cc7af53d417
Carey Fong <vicofpf@outlook.com>
2015-11-03 11:48:51 +0800

    添加项目

---
+++ TracePanel/TracePanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TraceCtrlLib
+{
+    public class TracePanel : Panel
+    {
+        public TracePanel()
+        {
+            if(true)
+            {
+
+            }
+        }
+    }
+}
