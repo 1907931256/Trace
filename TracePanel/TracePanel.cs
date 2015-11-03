f9b3e9c2936d35dcd8c409cda78190582ab53203
Carey Fong <vicofpf@outlook.com>
2015-11-03 13:56:57 +0800

    增加OpenAtStyles

--- TracePanel/TracePanel.cs
+++ TracePanel/TracePanel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
+using System.Drawing.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +10,23 @@ using System.Windows.Forms;
 
 namespace TraceCtrlLib
 {
+    [Editor("System.Windows.Forms.Design.AnchorEditor, System.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
+    [Flags]
+    public enum OpenAtStyles
+    {
+        Top = 1,
+        Bottom = 2,
+        Left = 4,
+        Right = 8,
+    }
+
     public class TracePanel : Panel
     {
         public TracePanel()
         {
             if(true)
             {
-
-            }
+                            }
         }
 
         protected override void OnPaint(PaintEventArgs e)
