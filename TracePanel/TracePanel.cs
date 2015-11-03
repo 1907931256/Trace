02d6a382b7b66a776474a9de1828e7bec064b071
Carey Fong <vicofpf@outlook.com>
2015-11-03 13:31:57 +0800

    重绘方法

--- TracePanel/TracePanel.cs
+++ TracePanel/TracePanel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,16 @@ namespace TraceCtrlLib
             {
 
             }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics dc = e.Graphics;
+            Pen bluePen = new Pen(Color.Blue, 3);
+            dc.DrawRectangle(bluePen, 0, 0, 50, 50);
+            Pen redPen = new Pen(Color.Red, 2);
+            dc.DrawEllipse(redPen, 0, 50, 80, 60);
         }
     }
 }
